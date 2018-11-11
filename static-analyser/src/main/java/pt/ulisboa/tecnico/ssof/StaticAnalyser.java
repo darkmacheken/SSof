db9648f126cc68d7b9157ec9393be56b57f8bba3
mafsbaptista <mafa.ferreira96@gmail.com>
2018-11-11 16:02:28 +0000

    Setup log4j

--- static-analyser/src/main/java/pt/ulisboa/tecnico/ssof/StaticAnalyser.java
+++ static-analyser/src/main/java/pt/ulisboa/tecnico/ssof/StaticAnalyser.java
@@ -1,7 +1,14 @@
 package pt.ulisboa.tecnico.ssof;
 
+import org.apache.log4j.Logger;
+
+
 public class StaticAnalyser {
+	
+	final static Logger logger = Logger.getLogger(StaticAnalyser.class);
+
     public static void main( String[] args ) {
+    	logger.info("Starting the program.");
         System.out.println( "Hello World!" );
     }
 }
