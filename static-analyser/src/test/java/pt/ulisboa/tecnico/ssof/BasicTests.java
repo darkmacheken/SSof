e7abca1e239795e81199a2c499d11b4a9a3e1500
Leonardo Epifânio <leonardo.epifanio@tecnico.ulisboa.pt>
2018-11-15 18:22:08 +0000

    2: Auto stash before merge of "SSof-2" and "develop"

--- static-analyser/src/test/java/pt/ulisboa/tecnico/ssof/BasicTests.java
+++ static-analyser/src/test/java/pt/ulisboa/tecnico/ssof/BasicTests.java
@@ -9,19 +9,26 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
-public class ExampleTest {
+public class BasicTests {
 	
 	@SuppressWarnings("resource")
 	@Test
-	public void success() throws IOException, JSONException {
+	public void test01() throws IOException, JSONException {
 		InputStream is1 = this.getClass().getResourceAsStream("/public_basic_tests/01_gets_all.output.json");
 		java.util.Scanner s1 = new Scanner(is1).useDelimiter("\\A");
 	    String jsonData1 = s1.hasNext() ? s1.next() : "";
 	    
+	    String[] testString = new String[1];
+	    testString[0] = "/public_basic_tests/01_gets_all.json";
+	    
+	    StaticAnalyser.main(testString);
+	    
+	    /*
 		InputStream is2 = this.getClass().getResourceAsStream("/01_gets_all.result.json");
 		java.util.Scanner s2 = new Scanner(is2).useDelimiter("\\A");
 	    String jsonData2 = s2.hasNext() ? s2.next() : "";
 
 	    JSONAssert.assertEquals(jsonData1, jsonData2, JSONCompareMode.NON_EXTENSIBLE);
+	    */
 	}
 }
